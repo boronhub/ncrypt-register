d83b64670bc0b00514876b80c6b2f70c77e165af
Divyansh <divi.sunway@gmail.com>
2020-07-17 22:53:28 +0530

    upd8

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,30 @@
-$('input[value="coin"]').on("change", function () {
-  if (!this.checked) {
-    $(".coin").hide();
-    $('input[name="coin"').attr("value", "");
-  } else if (this.checked) {
-    $(".coin").show();
-  }
-});
-$('input[value="crin"]').on("change", function () {
-  if (!this.checked) {
+$(".crinbox").click(function () {
+  var cbox = $('input[value="crin"]');
+  cbox.prop("checked", !cbox.prop("checked"));
+  if (!cbox.prop("checked")) {
     $(".crin").hide();
     $('input[name="crin"').attr("value", "");
-  } else if (this.checked) {
+  } else if (cbox.prop("checked")) {
     $(".crin").show();
   }
 });
-$('input[value="prog"]').on("change", function () {
-  if (!this.checked) {
-    $(".prog").hide();
-    $('input[name="pp"').attr("value", "");
-  } else if (this.checked) {
-    $(".prog").show();
-  }
-});
-$('input[value="quiz"]').on("change", function () {
-  if (!this.checked) {
+$(".quizbox").click(function () {
+  var cbox = $('input[value="quiz"]');
+  cbox.prop("checked", !cbox.prop("checked"));
+  if (!cbox.prop("checked")) {
     $(".quiz").hide();
     $('input[name="quiz"').attr("value", "");
-  } else if (this.checked) {
+  } else if (cbox.prop("checked")) {
     $(".quiz").show();
   }
 });
-$(".crinbox").click(function () {
-  var cbox = $('input[value="crin"]');
-  if (!cbox.checked) {
-    cbox.attr("checked", true);
-  } else if (cbox.checked) {
-    cbox.attr("checked", false);
+$(".ppbox").click(function () {
+  var cbox = $('input[value="prog"]');
+  cbox.prop("checked", !cbox.prop("checked"));
+  if (!cbox.prop("checked")) {
+    $(".prog").hide();
+    $('input[name="pp"').attr("value", "");
+  } else if (cbox.prop("checked")) {
+    $(".prog").show();
   }
 });
